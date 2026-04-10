767b5b38b9f7213c37469005fb93b2bfdabd43e0
Karol3ke36 <karolpucyk9@gmail.com>
2026-04-10 08:58:50 +0200

    bok

---
+++ app/src/main/java/com/example/zegar_szachowy/Gracz.java
@@ -0,0 +1,12 @@
+package com.example.zegar_szachowy;
+
+import android.os.BugreportManager;
+import android.os.CountDownTimer;
+import android.widget.Button;
+
+public class Gracz {
+    private int liczbaSekund;
+    private boolean czyAktywny;
+    private CountDownTimer countDownTimer;
+    private Button button;
+}
